23e028be1ac4f9adaa013621eb703465961ee5e0
816678 <816678@unizar.es>
2023-04-26 14:16:36 +0200

    Generación de expresiones y asignaciones

---
+++ practica_4/lib/attributes/Attributes.java
@@ -0,0 +1,45 @@
+//*****************************************************************
+// File:   Attributes.java
+// Author: Procesadores de Lenguajes-University of Zaragoza
+// Date:   enero 2022
+//         Clase única para almacenar los atributos que pueden aparecer en el traductor de adac
+//         Se hace como clase plana, por simplicidad. Los atributos que se pueden almacenar
+//         se autoexplican con el nombre
+//*****************************************************************
+
+package lib.attributes;
+import lib.symbolTable.*;
+import java.util.ArrayList;
+import java.util.AbstractMap.SimpleEntry;
+
+public class Attributes implements Cloneable {
+    public Symbol.Types type;
+    public Symbol.ParameterClass parClass;
+
+    public int valInt;
+    public boolean valBool;
+    public char valChar;
+    public String valString;
+    //COMPLETAR
+
+    public Attributes() {
+        //COMPLETAR
+    }
+
+    public Attributes clone() {
+    	try {
+    		return (Attributes) super.clone();
+    	}
+    	catch (CloneNotSupportedException e) {
+    		return null; 
+    	}
+    }
+
+    public String toString() {
+        return
+            "type = " + type + "\n" +
+            "parClass = " + parClass + "\n" ;
+            //COMPLETAR
+        
+    }
+}
---
+++ test_propios/prueba1.cl
@@ -0,0 +1,11 @@
+
+
+
+void main()
+{
+    int n[2];
+    int i;
+
+    i = 1;
+    n[0] = i*2;
+}
